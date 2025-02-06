59f8b516c1f1b7bd37a2a8a4405086dcef7c3280
Didier Viret <didier.viret@orif.ch>
2025-02-06 11:37:59 +0100

    style(frontend) : place logo and title in the banner

---
+++ frontend/src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Logo from '../ui/logo'
+import Title from '../ui/title'
+
+function Banner() {
+    return (<>
+        <Logo />
+        <Title className="pt-4 sm:pt-6 ">La maison de la jungle</Title>
+    </>)
+}
+
+export default Banner
--- frontend/src/layouts/main.jsx
+++ frontend/src/layouts/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Banner from '../components/Banner'
+
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (<>
       <div className="flex w-full h-full p-2 sm:p-8">
         <div className="flex w-full h-full justify-center items-center flex-col border-primary border-4 sm:border-8 p-2 sm:p-8">
+          <Banner />
           <Outlet />
         </div>
       </div>
