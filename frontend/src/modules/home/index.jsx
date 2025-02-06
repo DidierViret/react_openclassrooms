6e7cdcf3b470cd7210e289b104a8e85f7aa380d9
Didier Viret <didier.viret@orif.ch>
2025-02-06 11:44:10 +0100

    feat(frontend): display cart in the homepage

--- frontend/src/modules/home/index.jsx
+++ frontend/src/modules/home/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import Link from '../../ui/link'
+import Cart from '../../components/Cart'
 
 const Home = () => {
   return (<>
+    <Cart />
     <Link className="pt-4 sm:pt-6" to="contact">Contact</Link>
   </>)
 }
